a2e71529161b5931445412818d3198c549106da8
Liuyonghui <sdjllyh@gmail.com>
2012-10-16 21:38:24 +0800

    none

--- vps_install/install.sh
+++ vps_install/install.sh
@@ -1,5 +1,10 @@
+aptitude update
 aptitude install screen
 screen -S installsystem
+
+
+
+
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 dpkg-reconfigure locales
 
@@ -11,12 +16,12 @@ aptitude update
 apt-get upgrade
 aptitude upgrade
 
-aptitude install git gcc nginx mysql-client mysql-server
-aptitude install spawn-fcgi
+aptitude install git gcc nginx mysql-client mysql-server spawn-fcgi
 aptitude install python-webpy python-mysqldb python-setuptools python-imaging python-httplib2 python-memcache
 easy_install pip
 aptitude install php5 php5-fpm php5-mysql
-aptitude install sphinxsearch imagemagick memcached
+aptitude install sphinxsearch imagemagick memcached heirloom-mailx exim4
+aptitude install curl vnstat vim zsh
 
 # set beijing date
 cp /etc/localtime /etc/localtime.bak
@@ -26,9 +31,8 @@ ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 rm /etc/nginx/sites-enabled/default
 cp /etc/php5/fpm/pool.d/www.conf /etc/php5/fpm/pool.d/www.conf.bak
 cp conf/php5-fpm-www-conf /etc/php5/fpm/pool.d/www.conf
+cp conf/close_default /etc/nginx/sites_enabled/
 /etc/init.d/nginx restart
 /etc/init.d/php5-fpm restart
 
-
-# exit screen
-exit
+echo "ok"
