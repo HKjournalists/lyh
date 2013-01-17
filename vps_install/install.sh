6f7340aca68c6403eecb91ebcfb608af2cea11c5
Liuyonghui <sdjllyh@gmail.com>
2013-01-17 12:01:36 +0800

    none

--- vps_install/install.sh
+++ vps_install/install.sh
@@ -22,8 +22,9 @@ aptitude install python-webpy python-mysqldb python-setuptools python-imaging py
 easy_install pip
 aptitude install php5 php5-fpm php5-mysql
 aptitude install sphinxsearch imagemagick memcached heirloom-mailx exim4
-aptitude install curl vnstat vim zsh unzip dstat telnet
+aptitude install curl vnstat vim zsh unzip dstat telnet ntpdate
 aptitude install vim-gnome vim-gui-common vim-full
+ntpdate 210.72.145.44
 
 # set beijing date
 cp /etc/localtime /etc/localtime.bak
@@ -36,5 +37,7 @@ cp conf/php5-fpm-www-conf /etc/php5/fpm/pool.d/www.conf
 cp conf/close_default /etc/nginx/sites_enabled/
 /etc/init.d/nginx restart
 /etc/init.d/php5-fpm restart
+
+echo "SET time_zone = '+8:00'" | mysql -uroot -p131400
 
 echo "ok"
